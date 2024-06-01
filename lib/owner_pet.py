34406f8bd4dd457f1639b81c99be9b8888664b01
casey <caseymwangi31@gmail.com>
2024-06-01 19:47:45 +0300

    to many lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,30 @@
 class Pet:
-    pass
+    
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    
+    def __init__(self,name,pet_type, owner = None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"{pet_type} is not a valid pet type")
+        self.name = name
+        self.owner =owner
+        self.pet_type = pet_type
+        Pet.all.append(self)
+        if owner:
+            owner.add_pet(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+    def pets(self):
+        return self._pets 
+    
+    def add_pet(self,pet):
+        if not isinstance(pet, Pet):
+            raise Exception(f"{pet} is not a valid pet")
+        self._pets.append(pet)
+        pet.owner = self
+        
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
